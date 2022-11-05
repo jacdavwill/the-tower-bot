486808fb9e90c2f02b5894b3622c4f438f7be141
Jacob Williams <jacdavwill@gmail.com>
2022-11-05 08:40:13 -0600

    transitioned to no ad pack and improved round playing

--- main.py
+++ main.py
@@ -10,9 +10,8 @@ from random import random
 class State(enum.Enum):
     PAUSED = 1
     QUITTING = 2
-
-    BATTLING = 10
-    VIEWING_AD = 11
+    STARTING = 10
+    BATTLING = 11
 
 
 class Tab(enum.Enum):
@@ -24,31 +23,26 @@ class Tab(enum.Enum):
 
 
 mouse = Controller()
-current_state = State.BATTLING
-past_state = State.BATTLING
+current_state = State.STARTING
+past_state = State.STARTING
 summary = {
     "gem_5_rewards_claimed": 0,
     "gem_2_rewards_claimed": 0,
     "start_time": time(),
-    "start_coins": "187.20K",
-    "start_gems": 1008,
-    "restarts": 0
+    "rounds": 0
 }
-GAME_SCREEN_REGION = (661, 41, 559, 988)  # left, top, width, height
+GAME_SCREEN_REGION = (660, 41, 560, 988)  # left, top, width, height
+TAB_SCREEN_REGION = (660, 971, 560, 60)
 
 # Times
-ad_start_time = time()
 gem_5_start_time = 0
 gem_2_start_time = 0
 round_time = time()
 
 # Intervals (seconds)
-ad_int = 35
-ad_max_timeout_int = 60
 gem_5_int = 10 * 60  # 10 minutes
 gem_2_int = 15 * 60  # 15 minutes
-sprint_t6_int = 37
-restart_int = 20
+restart_int = 10
 
 # Assets
 ASSETS_PREFIX = "./assets/"
@@ -61,9 +55,8 @@ GEM_2_BUTTONS = [f"gem_2_button_{x}" for x in range(4)]
 RETRY_BUTTON = "retry_button"
 UTILITY_TAB_DISACTIVATED = "utility_tab_disactivated"
 UTILITY_TAB_ACTIVATED = "utility_tab_activated"
-BLUESTACKS_BACK_BUTTON = "bluestacks_back_button"
-BLUESTACKS_HOME_BUTTON = "bluestacks_home_button"
-ROUND_REWARD_BUTTON = "round_reward_button"
+# BLUESTACKS_BACK_BUTTON = "bluestacks_back_button"
+# BLUESTACKS_HOME_BUTTON = "bluestacks_home_button"
 THE_TOWER_IMGS = ["the_tower_img", "the_tower_img_larger"]
 
 # Colors
@@ -100,64 +93,40 @@ def on_release(key):
 
 def change_state(new_state):
     global current_state, past_state
-    print("State: ", new_state)
     past_state = current_state
     current_state = new_state
 
 
-def click(pos):
+def click(pos, clicks=1):
     mouse.position = pos
-    mouse.click(button=Button.left)
-    sleep(.5)
+    mouse.click(button=Button.left, count=clicks)
+    sleep(.25)
 
 
-def find_img(img_file_name, confidence=None, full_screen=False):
+def find_img(img_file_name, confidence=None, full_screen=False, region=GAME_SCREEN_REGION):
     img_path = ASSETS_PREFIX + img_file_name + ASSETS_FILE_TYPE
     if confidence is None:
         if full_screen:
             return pyautogui.locateCenterOnScreen(img_path)
         else:
-            return pyautogui.locateCenterOnScreen(img_path, region=GAME_SCREEN_REGION)
+            return pyautogui.locateCenterOnScreen(img_path, region=region)
     else:
-        return pyautogui.locateCenterOnScreen(img_path, confidence=confidence, region=GAME_SCREEN_REGION)
+        return pyautogui.locateCenterOnScreen(img_path, confidence=confidence, region=region)
 
 
 def check_gem_5():
-    global current_state, ad_start_time
-    # print("checking gem 5")
+    global gem_5_start_time
     pos = find_img(GEM_5_BUTTON)
     if pos is not None:
-        print("found 5 gem, starting ad: ", (time() - gem_5_start_time) / 60, " minutes")
         click(pos)
-        change_state(State.VIEWING_AD)
-        ad_start_time = time()
-
-
-def check_ad_closed():
-    global current_state, summary, gem_5_start_time
-    # print("checking ad closed")
-    pos = find_img(GEM_5_CLAIM_BUTTON)
-    if pos is not None:
-        print("found 5 gem claim, ad closed: ", time() - ad_start_time)
+        print("found 5 gem: ", (time() - gem_5_start_time) / 60, " minutes")
         summary["gem_5_rewards_claimed"] += 1
         gem_5_start_time = time()
-        click(pos)
-        change_state(State.BATTLING)
-        return True
 
-    pos = find_img(CLAIM_REWARD_BUTTON)
-    if pos is not None:
-        print("found 1.5x coin claim, ad closed: ", time() - ad_start_time)
-        click(pos)
-        change_state(State.BATTLING)
-        return True
-
-    return False
 
 
 def check_gem_2():
     global gem_2_start_time
-    # print("checking gem 2")
     for img in GEM_2_BUTTONS:
         pos = find_img(img, 0.7)
         if pos is not None:
@@ -165,32 +134,20 @@ def check_gem_2():
             print("found 2 gem: ", (time() - gem_2_start_time) / 60, " minutes")
             summary["gem_2_rewards_claimed"] += 1
             gem_2_start_time = time()
-            break
 
 
 def check_game_over():
-    global current_state, round_time, ad_start_time
-    # print("checking game over")
+    global current_state, round_time, summary
     pos = find_img(RETRY_BUTTON)
     if pos is not None:
-        t = time() - round_time
-        if t > 3 * ad_int:
-            reward_pos = find_img(ROUND_REWARD_BUTTON)
-            if reward_pos is not None:
-                print("found game over, watching reward ad")
-                click(reward_pos)
-                change_state(State.VIEWING_AD)
-                ad_start_time = time()
-                sleep(1)
-                return True
-
-        print("found game over, restarting")
+        round_time = time()
+        summary["rounds"] += 1
         click(pos)
-        change_state(State.BATTLING)
+        change_state(State.STARTING)
 
 
 def get_current_tab():
-    pos = find_img(UTILITY_TAB_ACTIVATED)
+    pos = find_img(UTILITY_TAB_ACTIVATED, region=TAB_SCREEN_REGION)
     if pos is not None:
         return Tab.UTILITY
     else:
@@ -198,76 +155,61 @@ def get_current_tab():
 
 
 def play_round():
-    # print("playing round")
-    current_tab = get_current_tab()
+    global current_state
+    if current_state == State.STARTING:
+        current_tab = get_current_tab()
+    else:
+        current_tab = Tab.UTILITY
+
     if current_tab is Tab.UTILITY:
         if True:
-            click((1173, 868))  # coins/wave
+            click((1173, 868), clicks=5)  # coins/wave
         else:
             click((823, 868))  # coin/kill bonus
     else:
-        # print("activating utility tab")
-        pos = find_img(UTILITY_TAB_DISACTIVATED)
+        pos = find_img(UTILITY_TAB_DISACTIVATED, region=TAB_SCREEN_REGION)
         if pos is not None:
             click(pos)
             sleep(1)
-        else:
-            print("uh-oh, bad state")
-            return False
 
 
-def exit_ad():
-    global ad_start_time
-    print("exiting ad")
-    pos = find_img(BLUESTACKS_BACK_BUTTON, full_screen=True)
-    if pos is not None:
-        click(pos)
-        sleep(2)
-
-
-def reset_game():
-    global current_state, summary
-    print("resetting game")
-    pos = find_img(BLUESTACKS_HOME_BUTTON, full_screen=True)
-    if pos is not None:
-        click(pos)
-        sleep(5)
-        for img in THE_TOWER_IMGS:
-            pos_tower = find_img(img, full_screen=True)
-            if pos_tower is not None:
-                click(pos_tower)
-                summary["restarts"] += 1
-                sleep(5)
+# def reset_game():
+#     global current_state, summary
+#     print("resetting game")
+#     pos = find_img(BLUESTACKS_HOME_BUTTON, full_screen=True)
+#     if pos is not None:
+#         click(pos)
+#         sleep(5)
+#         for img in THE_TOWER_IMGS:
+#             pos_tower = find_img(img, full_screen=True)
+#             if pos_tower is not None:
+#                 click(pos_tower)
+#                 summary["restarts"] += 1
+#                 sleep(5)
 
 
 def play():
-    global current_state, ad_start_time, ad_int
+    global current_state
     listener = keyboard.Listener(
         on_press=on_press,
         on_release=on_release)
     listener.start()
-    print("Starting in state: ", current_state)
+
     while current_state is not State.QUITTING:
         # sleep(1)
         # print('Mouse: {0}, Color: {1}'.format(mouse.position, pyautogui.pixel(*mouse.position)))
         if current_state is not State.PAUSED:
             t = time()
-            if current_state is State.BATTLING:
-                if t - round_time > restart_int:
-                    check_game_over()
-                if t - gem_2_start_time > gem_2_int:
-                    check_gem_2()
-                if t - gem_5_start_time > gem_5_int:
-                    check_gem_5()
-                play_round()
-            elif current_state is State.VIEWING_AD:
-                # print("viewing add time: ", t - ad_start_time)
-                if t - ad_start_time > ad_max_timeout_int:
-                    reset_game()
-                if t - ad_start_time > ad_int:
-                    if not check_ad_closed():
-                        exit_ad()
+            if t - round_time > restart_int:
+                check_game_over()
+            if t - gem_2_start_time > gem_2_int:
+                check_gem_2()
+            if t - gem_5_start_time > gem_5_int:
+                check_gem_5()
+            play_round()
 
 
-# change_state(State.VIEWING_AD)
 play()
+
+# 7.95, 8.56, 7.78 = 8.1 c/w
+#
